5451db487609b71a2e7c1459b0acef5b899a09fd
Michael Langat <langatkipmichael@gmail.com>
2022-08-14 15:03:08 +0300

    added Balance, IncomeExpense,Header components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 
 import {Header} from "./components/Header";
+import {Balance} from "./components/Balance";
+import {IncomeExpenses} from "./components/IncomeExpenses";
 
 
 
 function App() {
   return (
-   <Header/>
+    <div>
+      <Header/>
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses/>
+
+      </div>
+
+    </div>
+   
   );
 }
 
---
+++ src/components/Balance.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const Balance = () => {
+    return (
+        <>
+            <h4>Your Balance</h4>
+            <p id="balance">$0.00</p>
+        </>
+    )
+}
