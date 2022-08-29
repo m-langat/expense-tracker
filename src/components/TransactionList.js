64f88a03ca1dbd598781acc75b8f690370cbe353
Michael Langat <langatkipmichael@gmail.com>
2022-08-29 15:57:55 +0300

    created transaction list

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, {useContext} from 'react'
+
+import {GlobalContext} from '../Context/GlobalState'
 
 export const TransactionList = () => {
+  const {transactions} = useContext(GlobalContext)
   return (
     <div>
         <h4>Transaction History</h4>
         <ul className="list">
+          {transactions.map(transction =>())}
             <li className="minus"> 
             Cash <span>-400</span><button className="btn-delete">x</button>
             </li>
